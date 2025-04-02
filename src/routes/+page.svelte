f6a8d66e6a855c2df8f1995c0bbffc8d3416b7e2
harvanchik <66141166+harvanchik@users.noreply.github.com>
2025-04-02 00:01:06 -0500

    added hello world page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,14 @@
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Svelte Tailwind Template</title>
+	<meta name="description" content="Svelte Tailwind Template" />
 </svelte:head>
 
 <section>
-	<h1>Hello, World!</h1>
+	<div class="min-h-screen flex items-center justify-center relative">
+		<div class="text-center relative z-10 px-4">
+			<h1 class="text-6xl md:text-8xl font-bold text-black/95">Hello World</h1>
+		</div>
+	</div>
 </section>
