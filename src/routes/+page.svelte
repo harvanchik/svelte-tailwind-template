5833f27d91649f78ec281d1f2190f052e8fd8530
harvanchik <66141166+harvanchik@users.noreply.github.com>
2025-04-02 16:13:19 -0500

    give text gradient

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,11 @@
 <section>
 	<div class="min-h-screen flex items-center justify-center relative">
 		<div class="text-center relative z-10 px-4">
-			<h1 class="text-6xl md:text-8xl font-bold text-black/95">Hello World</h1>
+			<h1
+				class="text-6xl md:text-8xl font-bold bg-gradient-to-r from-blue-400 via-fuchsia-400 to-violet-400 inline-block text-transparent bg-clip-text"
+			>
+				Hello World
+			</h1>
 		</div>
 	</div>
 </section>
